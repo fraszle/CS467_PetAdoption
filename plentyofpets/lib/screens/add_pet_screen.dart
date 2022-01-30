5267397b0dbbd12af436189570f85e21266b7136
Johan Park <parkjohan@gmail.com>
2022-01-30 11:00:01 -1000

    Updated Admin Homepage UI
    made the Admin homepage UI cleaner, and created sample add_pet_screen as a blank page that the buttons navigate to

---
+++ plentyofpets/lib/screens/add_pet_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AddPetScreen extends StatelessWidget {
+  const AddPetScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
