66cde69ba50a0ff09a21665e07b63df2a582ff6a
Jeremy Marr <jmarr445@gmail.com>
2022-02-02 18:27:16 -0600

    Added Firebase Auth to the login form

--- plentyofpets/lib/components/login_form.dart
+++ plentyofpets/lib/components/login_form.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:plentyofpets/theme.dart';
 
 import '../main.dart';
 
@@ -12,8 +14,13 @@ class AuthenticationHandler extends StatefulWidget {
 }
 
 class AuthenticationHandlerState extends State<AuthenticationHandler> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
   final controller = TextEditingController();
+
   bool showLoginForm = false;
+  String? email;
+  String? password;
 
   Widget authenticationButtons() {
     /**
@@ -48,49 +55,74 @@ class AuthenticationHandlerState extends State<AuthenticationHandler> {
      * Form that lets the user login.
      */
     return Form(
+        key: _formKey,
         child: Column(
-      children: [
-        Padding(
-            padding: const EdgeInsets.all(10),
-            child: TextFormField(
-              decoration: const InputDecoration(
-                  border: UnderlineInputBorder(), labelText: 'Username'),
-            )),
-        Padding(
-            padding: const EdgeInsets.all(10),
-            child: TextFormField(
-              obscureText: true,
-              enableSuggestions: false,
-              decoration: const InputDecoration(
-                  border: UnderlineInputBorder(), labelText: 'Password'),
-            )),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      showLoginForm = false;
-                    });
-                  },
-                  child: const Icon(Icons.arrow_back),
-                )),
-            Flexible(
-                flex: 2,
-                child: OutlinedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyApp.homeRoute);
-                    },
-                    child: Text('login',
-                        style: Theme.of(context).textTheme.button))),
-            const Flexible(flex: 1, fit: FlexFit.tight, child: SizedBox())
+            emailField(),
+            passwordField(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          showLoginForm = false;
+                        });
+                      },
+                      child: const Icon(Icons.arrow_back),
+                    )),
+                Flexible(
+                    flex: 2,
+                    child: OutlinedButton(
+                        onPressed: submitCredentials,
+                        child: Text('login',
+                            style: Theme.of(context).textTheme.button))),
+                const Flexible(flex: 1, fit: FlexFit.tight, child: SizedBox())
+              ],
+            )
           ],
-        )
-      ],
-    ));
+        ));
+  }
+
+  Widget emailField() {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: TextFormField(
+          decoration: const InputDecoration(
+              errorStyle: PlentyOfPetsTheme.formErrorText,
+              border: UnderlineInputBorder(),
+              labelText: 'Email'),
+          onSaved: (value) => email = value,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter an email address';
+            }
+            return null;
+          },
+        ));
+  }
+
+  Widget passwordField() {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: TextFormField(
+          obscureText: true,
+          enableSuggestions: false,
+          decoration: const InputDecoration(
+              errorStyle: PlentyOfPetsTheme.formErrorText,
+              border: UnderlineInputBorder(),
+              labelText: 'Password'),
+          onSaved: (value) => password = value,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter a password';
+            }
+            return null;
+          },
+        ));
   }
 
   @override
@@ -98,5 +130,33 @@ class AuthenticationHandlerState extends State<AuthenticationHandler> {
     return FractionallySizedBox(
         widthFactor: 0.8,
         child: showLoginForm ? loginForm() : authenticationButtons());
+  }
+
+  void submitCredentials() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+    try {
+      await auth.signInWithEmailAndPassword(email: email!, password: password!);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "user-not-found":
+          print('No user found for that email.');
+          break;
+        case "wrong-password":
+          print('Wrong password provided for that user.');
+          break;
+        case "invalid-email":
+          print('Invalid email address provided.');
+          break;
+        default:
+          print('An unknown error occured.');
+      }
+      return;
+    }
+
+    Navigator.pushNamed(context, MyApp.homeRoute);
   }
 }
