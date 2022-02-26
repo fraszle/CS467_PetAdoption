0f8a0137599cacd3bfc851c0f6394ad7e2ef4489
OSUBass <bassr@oregonstate.edu>
2022-02-26 12:47:52 -0600

    favorites are pulled from firestore to persist

--- plentyofpets/lib/components/pet_card.dart
+++ plentyofpets/lib/components/pet_card.dart
@@ -3,6 +3,7 @@ import 'package:plentyofpets/components/extract_pet_basics.dart';
 import 'package:plentyofpets/components/pet_profile_args.dart';
 import 'package:favorite_button/favorite_button.dart';
 import 'package:plentyofpets/services/pet_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PetCard extends StatefulWidget {
   const PetCard(this.doc, this.petID, {Key? key}) : super(key: key);
@@ -15,84 +16,106 @@ class PetCard extends StatefulWidget {
 }
 
 class _PetCardState extends State<PetCard> {
+  //stream for current user doc
+  final Stream<DocumentSnapshot> userStream = 
+    FirebaseFirestore.instance.collection('users').
+    doc('${DatabaseService().getUser()}').snapshots();
+
+  bool isFav = false;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        // When a Card is tapped, we navigate to that pet's profile
-        onTap: () {
-          Navigator.pushNamed(context, ExtractPetBasics.routeName,
-              arguments: PetProfileArgs(widget.petID, widget.doc));
-        },
-        // Create a card with a ListTile that has pet info
-        child: Card(
-          elevation: 80,
-          color: Color.fromARGB(255, 219, 240, 220),
-          shadowColor: Color.fromARGB(255, 1, 27, 1),
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            height: 180,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-              Column(children:[
-                Text(widget.doc['name'], style: const TextStyle(
-                  fontFamily: 'IndieFlower',
-                  fontSize:32,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 248, 121, 17))),
-                Expanded(
-                  child: Container(
-                    height:110,
-                    child:CircleAvatar(
-                      backgroundColor: Color.fromARGB(255, 5, 173, 41),
-                      radius: 70,
-                      child:CircleAvatar(
-                        radius: 55,
-                        backgroundImage: NetworkImage(widget.doc['mainPhoto'] ??
-                          'https://images.unsplash.com/photo-1636654129379-e7ae6f30bfd0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'), // Temp image in case pet doesn't have mainPhoto
+    return StreamBuilder<DocumentSnapshot>(
+      stream: userStream,
+      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+         if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
+        List favPets = snapshot.data!['favs'];
+        
+        if(favPets.contains("${widget.petID}")){
+          isFav = true;
+        }
+        return GestureDetector(
+            // When a Card is tapped, we navigate to that pet's profile
+            onTap: () {
+              Navigator.pushNamed(context, ExtractPetBasics.routeName,
+                  arguments: PetProfileArgs(widget.petID, widget.doc));
+            },
+            // Create a card with a ListTile that has pet info
+            child: Card(
+              elevation: 80,
+              color: Color.fromARGB(255, 219, 240, 220),
+              shadowColor: Color.fromARGB(255, 1, 27, 1),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                height: 180,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                  Column(children:[
+                    Text(widget.doc['name'], style: const TextStyle(
+                      fontFamily: 'IndieFlower',
+                      fontSize:32,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 248, 121, 17))),
+                    Expanded(
+                      child: Container(
+                        height:110,
+                        child:CircleAvatar(
+                          backgroundColor: Color.fromARGB(255, 5, 173, 41),
+                          radius: 70,
+                          child:CircleAvatar(
+                            radius: 55,
+                            backgroundImage: NetworkImage(widget.doc['mainPhoto'] ??
+                              'https://images.unsplash.com/photo-1636654129379-e7ae6f30bfd0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'), // Temp image in case pet doesn't have mainPhoto
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              ]),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                Text(widget.doc['availability'],style: const TextStyle(
-                  fontFamily: 'Lato',
-                  fontSize:23,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 20, 77, 23))),
-                Text('Type: ${widget.doc['type']}', style: const TextStyle(
-                  fontFamily: 'Lato',
-                  fontSize:23,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 20, 77, 23))),
-                Text('Breed: ${widget.doc['breed']}', style: const TextStyle(
-                  fontFamily: 'Lato',
-                  fontSize:23,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 20, 77, 23)))
-              ],),
-              
-                Align(
-                  alignment: Alignment.topRight,
-                  child:FavoriteButton(
-                    isFavorite:false,
-                    iconSize: 50,
-                    // iconDisabledColor: Colors.white,
-                    valueChanged: (_isFavorite) {
-                      if(_isFavorite){
-                        DatabaseService().addFav(widget.petID);
-                      }else{ 
-                        DatabaseService().deleteFav(widget.petID);}
-                    }
-                  )    
-                ),
-            ],)
-          ),
-        ));
+                  ]),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                    Text(widget.doc['availability'],style: const TextStyle(
+                      fontFamily: 'Lato',
+                      fontSize:23,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 20, 77, 23))),
+                    Text('Type: ${widget.doc['type']}', style: const TextStyle(
+                      fontFamily: 'Lato',
+                      fontSize:23,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 20, 77, 23))),
+                    Text('Breed: ${widget.doc['breed']}', style: const TextStyle(
+                      fontFamily: 'Lato',
+                      fontSize:23,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 20, 77, 23)))
+                  ],),
+                  
+                    Align(
+                      alignment: Alignment.topRight,
+                      child:FavoriteButton(
+                        isFavorite:isFav,
+                        iconSize: 50,
+                        valueChanged: (_isFavorite) {
+                          //setState((){isFav = _isFavorite;});
+                          if(_isFavorite){
+                            DatabaseService().addFav(widget.petID);
+                          }else{ 
+                            DatabaseService().deleteFav(widget.petID);}
+                        }
+                      )    
+                    ),
+                ],)
+              ),
+            ));
+      }
+    );
   }
 }
